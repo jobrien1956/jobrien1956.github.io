53f4bdc4489e5a5a23ca1372c40ae103db1a2faf
Jim OBrien <jobrien1956@gmail.com>
2019-10-09 17:14:52 -0400

    Add to data and output 1 & 2

--- bin/app.js
+++ bin/app.js
@@ -41,21 +41,27 @@ HA.views.Adventure = function (vnode) {
 
 var myData = {
 	"grpMember": {
-
 		"hrefx": "201908sierra.html",
-		"name": "from haPages Aug19 Sierra",
-		"storeName": "sierra"
+		"name": "Aug19 Sierra",
+		"storeName": "sierra",
+		"TripId": "ha-201908sierra",
+		"id": "ha-sierra",
+		"BackgroundPic": "img/index/201908sierra.jpg",
+		"PageLink": "adventures/201908sierra.html",
+		"PageTitle": "June 2019 - Sierra Nevada",
+		"PageSubtitle": "",
+  	"TripSumm": "A five day, 45 mile hike through the Benson Lake Loop"
 	}
-
 }
 
 
 var Layout = {
 	view: function(vnode) {
 		return m(".layout", [
-			m('div', {onclick: (e) => console.log('clicked', e)}, 'header'),
+			m('nav', {onclick: (e) => console.log('clicked', e)}, 'HavingAdventures NAV'),
+			m('h2', {onclick: (e) => console.log('clicked', e)}, 'HavingAdventures header'),
 			vnode.children,
-			m('div', 'footer'),
+			m('div', 'HavingAdventures footer'),
 		])
 	}
 }
@@ -77,6 +83,67 @@ const Trip = {
 		))
 	}
 }
+
+const Output1 = {
+	view: function(vnode) {
+		return m(".layout",  m("div", {"id":"output1"},
+			[
+				"TD List",
+				m("br"),
+				m("br"), " TripId = "+ myData.grpMember.TripId,
+				m("div"), " id = "+ myData.grpMember.id,
+				m("div"), " Background Pic = "+ myData.grpMember.BackgroundPic,
+				m("div"), " Page Link = "+ myData.grpMember.PageLink,
+				m("h2"), " Page Title = " + myData.grpMember.PageTitle,
+				m("h3"), " Page Subtitle = "+ myData.grpMember.PageSubtitle,
+				m("p"), " Trip Summ = "+ myData.grpMember.TripSumm,
+				m("br")
+			]
+		))
+	}
+}
+
+const Output2 = {
+		view: function (vnode) {
+			var theStoreData;
+			theStoreData = new HAindex.stores.JsonLoader({
+				url: vnode.attrs.dataUrl,
+				name: vnode.attrs.storeName
+			});
+
+			theStoreData.load();
+
+			return {
+				view: function (vnode) {
+					return m("div", [adventure()]);
+
+					function tripCard(trip) {
+						return m(".col-xs-6.col-sm-4.col-md-3[id=" + trip.id + "-th]",
+							[
+								m(".margin-top"),
+								m("a.square-text[href=" + trip.pageLink + "]", trip.pageTitle),
+								m(".square-img", {style: {"background-image": "url(" + trip.tripThumb + ")"}})
+							]
+						);
+					}
+
+					function adventure() {
+						var theStoreData = HAindex.stores[vnode.attrs.storeName];
+						if (!theStoreData) return undefined;
+						console.log("1 From adventure theStoreData.haPages is", theStoreData.haPages);
+						return [
+							m(".container", m(".row", [
+								theStoreData.haPages.map(function (adventure) {
+									return adventure.TripId && tripCard(adventure);
+								})]))
+
+						]
+					}
+
+				}
+			}
+		}
+	}
 // example 1
 m.route(document.body, "/", {
 	"/": {
